4f7b179c96f0da6d85555ba8b72018d617101d67
pedrocruz2 <pedro.cruz@sou.inteli.edu.br>
2023-08-07 13:50:03 -0300

    update

--- exercicio1.py
+++ exercicio1.py
@@ -1,4 +1,4 @@
-def conta_palavras_unicas(x: str): # Pedro Henrique de Azeredo Coutinho Cruz
+def conta_palavras_unicas(x: str) -> int: # Pedro Henrique de Azeredo Coutinho Cruz
     palavras = x.split()  
     palavras_unicas = set(palavras)
     return len(palavras_unicas)
